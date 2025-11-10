e7dc56b46c5a35750265f7308731624bcdb1c599
MaplMaple <306590123@qq.com>
2025-11-11 01:59:10 +0800

    added many changes

--- Assets/Scripts/Dialogue/BarOpening.cs
+++ Assets/Scripts/Dialogue/BarOpening.cs
@@ -29,10 +29,10 @@ public class BarOpening : MonoBehaviour
     private void PlayOpeningDialogue()
     {
         var dialogTexts = new List<DialogData>();
-        dialogTexts.Add(new DialogData(originalString: "几天前，我收到了我的朋友木樨发来的一封信件。", character: "主角"));
-        dialogTexts.Add(new DialogData(originalString: "虽然没有寄出者的信息，但是我很确信这是她寄来的，因为随信而来的还有我在学生时代赠送给她的护身符。", character: "主角"));
-        dialogTexts.Add(new DialogData(originalString: "信的内容几乎完全由我看不懂的文字进行书写，但落款处留下了一个地址，指向雨城的一家小酒馆。", character: "主角"));
-        dialogTexts.Add(new DialogData(originalString: "为了调查这封信以及木樨发这封信给我的原因，我启程前往雨城。至于木樨那边到底发生了什么事，也许只有我到达酒馆之后才能知道了。", callback: () => { FadeInPlayer().Forget(); }, character: "主角"));
+        dialogTexts.Add(new DialogData(originalString: "几天前，我收到了我的朋友木樨发来的一封信件。", character: "信件"));
+        dialogTexts.Add(new DialogData(originalString: "虽然没有寄出者的信息，但是我很确信这是她寄来的，因为随信而来的还有我在学生时代赠送给她的护身符。", character: "信件"));
+        dialogTexts.Add(new DialogData(originalString: "信的内容几乎完全由我看不懂的文字进行书写，但落款处留下了一个地址，指向雨城的一家小酒馆。", character: "信件"));
+        dialogTexts.Add(new DialogData(originalString: "为了调查这封信以及木樨发这封信给我的原因，我启程前往雨城。至于木樨那边到底发生了什么事，也许只有我到达酒馆之后才能知道了。", callback: () => { FadeInPlayer().Forget(); }, character: "信件"));
         DialogManager.Show(dialogTexts);
     }
 
@@ -56,7 +56,7 @@ public class BarOpening : MonoBehaviour
         dialogTexts.Add(new DialogData(originalString: "门没锁……以木樨的性格会不锁门吗？说起来，她叫我来到底是为了什么呢……信上的地址没错，就是这里。不过这地方已经落灰了，木樨是遇到什么事了吗？", character: "主角"));
         dialogTexts.Add(new DialogData(originalString: "哟，看看谁来了，这不是我们的大作家嘛。", character: "???"));
         dialogTexts.Add(new DialogData(originalString: "谁在说话？", character: "主角"));
-        dialogTexts.Add(new DialogData(originalString: "等等，她是...", character: "主角", callback: () => { PlayerWalkToFridge().Forget(); }));
+        dialogTexts.Add(new DialogData(originalString: "（等等，她是...）", character: "主角", callback: () => { PlayerWalkToFridge().Forget(); }));
         dialogTexts.Add(new DialogData(originalString: "我脑子里的问题现在越来越多了。说说吧，为什么你会在这里？木樨呢？", character: "主角"));
         dialogTexts.Add(new DialogData(originalString: "哼，你还是跟以前一样，老同学多年不见，不打声招呼就算了，态度还这么冷淡……", character: "???"));
         dialogTexts.Add(new DialogData(originalString: "没错，眼前这位名叫阿枫的人，她和木樨一样，是我在学生时代的好友，毕业后我们的关系依然很好。不过，此时此刻，为什么她会在木樨的酒馆里呢？", character: "主角"));
@@ -65,7 +65,7 @@ public class BarOpening : MonoBehaviour
         dialogTexts.Add(new DialogData(originalString: "/emote:Normal/我知道的……只有木樨失踪了。", character: "阿枫"));
         dialogTexts.Add(new DialogData(originalString: "什么！？", character: "主角"));
         dialogTexts.Add(new DialogData(originalString: "有好几个月了……我给她发消息不回，打电话不接，酒馆也一直关门。就在我以为她真的人间蒸发的时候，也收到了一封和她寄给你类似的信。", character: "阿枫"));
-        dialogTexts.Add(new DialogData(originalString: "我们都收到了信。这不是巧合，是某种邀请？或者说，是求救信号？", character: "主角"));
+        dialogTexts.Add(new DialogData(originalString: "（我们都收到了信。这不是巧合，是某种邀请？或者说，是求救信号？）", character: "主角"));
         dialogTexts.Add(new DialogData(originalString: "所以你才会来这里。", character: "主角"));
         dialogTexts.Add(new DialogData(originalString: "/emote:Speak/不然呢？总不能干等着。这地方虽然又小又破，但好歹是木樨开的，我想着或许能找到点什么线索。可惜，如你所见，除了满屋子的灰，也没有别的东西了。", character: "阿枫"));
         dialogTexts.Add(new DialogData(originalString: "这里的一切，都和她失踪前一样吗？", character: "主角"));
@@ -77,11 +77,13 @@ public class BarOpening : MonoBehaviour
         // TODO: 播放"噼啪"的电流声音效，画面彻底变暗
         dialogTexts.Add(new DialogData(originalString: "……停电了。", character: "阿枫"));
         dialogTexts.Add(new DialogData(originalString: "我去楼顶看看，可能是太阳能电力系统出了问题。你在这里别乱跑。", character: "主角"));
-        dialogTexts.Add(new DialogData(originalString: "/emote:Shock/喂，你一个人去？现在都几点了，楼顶黑灯瞎火的。", character: "阿枫"));
+        dialogTexts.Add(new DialogData(originalString: "/emote:Shock/喂，你一个人去？现在都几点了，外面黑灯瞎火的。", character: "阿枫"));
         dialogTexts.Add(new DialogData(originalString: "没事，我很快就回来。", character: "主角"));
         // TODO: 阿枫走到冰箱前面，切换为后背sprite，过了一会，向主角走来
         dialogTexts.Add(new DialogData(originalString: "/emote:Speak/拿上这个吧。最近雨城大大小小的有一系列怪事，注意安全哦。", character: "阿枫"));
-        dialogTexts.Add(new DialogData(originalString: "放心吧，我只是上去看看而已。", character: "主角", callback: () => { LoadScene(); }));
+        dialogTexts.Add(new DialogData(originalString: "获得武器【菜刀】", character: "菜刀"));
+        dialogTexts.Add(new DialogData(originalString: "这把刀很锋利，也许可以用来防身。（按X进行攻击）", character: "主角"));
+        dialogTexts.Add(new DialogData(originalString: "放心吧，我只是上去看看而已，大概不会有什么问题吧。", character: "主角", callback: () => { LoadScene(); }));
         DialogManager.Show(dialogTexts);
     }
 
